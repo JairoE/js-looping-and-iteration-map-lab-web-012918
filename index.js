4d11f67a22d6dff095fecb23beeaa1b1b24df23b
github username <github email address>
2018-03-12 14:25:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(arr) {
+  let x = arr.map(function(el) {return el.toLowerCase();});
+  return x;
+}
+
+function nameToAttributes(arr)  {
+  let newAr = arr.map(function(el) {
+    let i = el.indexOf(" ");
+    return {firstName: el.slice(0,i), lastName: el.slice(i+1)}
+  });
+  return newAr;
+}
+
+function attributesToPhrase(arr) {
+  let newAr = arr.map(function(el) {
+    return `${el.name} is from ${el.hometown}`;
+  })
+  return newAr;
+}
